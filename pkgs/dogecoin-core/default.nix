f4335fc3ee292f8d5384e158c8744109f9347378
Stephen Quoll <stephenquoll@gmail.com>
2024-05-22 17:16:49 +1000

    Added recursive keyword for version variable

--- pkgs/dogecoin-core/default.nix
+++ pkgs/dogecoin-core/default.nix
@@ -1,19 +1,23 @@
 { pkgs, stdenv, fetchurl, lib, ... }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "dogecoin-core";
   version = "1.14.7";
+
   src = fetchurl {
     url = "https://github.com/dogecoin/dogecoin/archive/refs/tags/v${version}.tar.gz";
     hash = "sha256-x0fCXGTb8pMa5vGKKkLlKXrTvj8qFX9X6KbiaHpnmF0=";
   };
+
   configureFlags = [
     "--with-incompatible-bdb"
     "--with-boost-libdir=${pkgs.boost}/lib"
   ];
+
   nativeBuildInputs = [
     pkgs.pkg-config
   ];
+
   buildInputs = [
     pkgs.autoreconfHook
     pkgs.openssl
@@ -26,6 +30,7 @@ stdenv.mkDerivation {
     pkgs.protobuf
     pkgs.qrencode
   ];
+
   meta = with lib; {
     description = "Allows anyone to operate a node in the Dogecoin blockchain networks";
     homepage = "https://dogecoin.com";
