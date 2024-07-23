d4016c95d2cd6a50b728b450b09860a55f37a05d
Stephen Quoll <stephenquoll@gmail.com>
2024-07-23 11:21:45 +1000

    Adding radicle binaries

---
+++ pkgs/radicle/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, stdenv, fetchurl, ... }:
+
+stdenv.mkDerivation rec {
+  pname = "radicle";
+  version = "1.0.0-rc.13";
+
+  src = fetchurl {
+    url = "https://files.radicle.xyz/releases/latest/radicle-1.0.0-rc.13-x86_64-unknown-linux-musl.tar.xz";
+    hash = "sha256-8l2O2DSLrdIGe3isQoS1MiJPT2MsBUIwiastFgHScIE=";
+  };
+
+  installPhase = ''
+    mkdir -p $out
+    cp -a * $out
+  '';
+
+  meta = {
+    description = "An open source, peer-to-peer code collaboration stack built on Git.";
+    homepage = "https://radicle.xyz";
+    changelog = "https://radicle.xyz/#updates";
+  };
+}
