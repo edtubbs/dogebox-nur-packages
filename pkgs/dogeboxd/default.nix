28380e3cb078b3be53c8bed14f0bf225dfec2c83
Adam Brady <adam@boxxen.org>
2024-09-06 11:27:50 +1000

    Clone dpanel into dogeboxd derivative

--- pkgs/dogeboxd/default.nix
+++ pkgs/dogeboxd/default.nix
@@ -1,24 +1,36 @@
 { lib, pkgs, stdenv, fetchurl, buildGoModule, ... }:
 
+let
+  dpanel = fetchGit {
+    url = "https://github.com/dogeorg/dpanel.git";
+    rev = "cdb3660f42ad54d00bb8c469fbb89966c6c47e0c";
+  };
+in
+
 buildGoModule {
   pname = "dogeboxd";
   version = "0.1";
 
   src = fetchGit {
     url = "https://github.com/dogeorg/dogeboxd.git";
-    rev = "3c8c3eb426db7481e1965ed73c98b50840356689";
+    rev = "091874560ae9c8e4875461100bfd3b1e5daec106";
   };
 
-  vendorHash = "sha256-Py1kZ7gLg0naQ7UIpmS7WNVV2S/rz8aYBYmGeisSh8g=";
+  vendorHash = "sha256-Py1kZ7gLg0naQ7UIpmS7WNVV2S/rz8aYBYmGeisSh8g";
 
   buildPhase = ''
     make
   '';
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp build/dogeboxd $out/bin
+    mkdir -p $out/dogeboxd/bin
+    cp build/* $out/dogeboxd/bin/
+
+    mkdir -p $out/dpanel
+    cp -r ${dpanel}/. $out/dpanel/
   '';
+
+  doCheck = false;
 
   nativeBuildInputs = [
     pkgs.go_1_22
