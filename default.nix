a18f3caabb7e1120cba69f019436b4933213b617
Stephen Quoll <stephenquoll@gmail.com>
2024-09-23 14:57:59 +1000

    Adding rk3588-firmware

--- default.nix
+++ default.nix
@@ -14,15 +14,16 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  dkm           = pkgs.callPackage ./pkgs/dkm {};
-  dogeboxd      = pkgs.callPackage ./pkgs/dogeboxd {};
-  dogecoin-core = pkgs.callPackage ./pkgs/dogecoin-core {};
-  dogemap       = pkgs.callPackage ./pkgs/dogemap {};
-  dogenet       = pkgs.callPackage ./pkgs/dogenet {};
-  jampuppy      = pkgs.callPackage ./pkgs/jampuppy {};
-  libdogecoin   = pkgs.callPackage ./pkgs/libdogecoin {};
-  nrpe          = pkgs.callPackage ./pkgs/nrpe {};
-  radicle       = pkgs.callPackage ./pkgs/radicle {};
-  radicle-httpd = pkgs.callPackage ./pkgs/radicle-httpd {};
-  gigawallet    = pkgs.callPackage ./pkgs/gigawallet {};
+  dkm             = pkgs.callPackage ./pkgs/dkm {};
+  dogeboxd        = pkgs.callPackage ./pkgs/dogeboxd {};
+  dogecoin-core   = pkgs.callPackage ./pkgs/dogecoin-core {};
+  dogemap         = pkgs.callPackage ./pkgs/dogemap {};
+  dogenet         = pkgs.callPackage ./pkgs/dogenet {};
+  jampuppy        = pkgs.callPackage ./pkgs/jampuppy {};
+  libdogecoin     = pkgs.callPackage ./pkgs/libdogecoin {};
+  nrpe            = pkgs.callPackage ./pkgs/nrpe {};
+  radicle         = pkgs.callPackage ./pkgs/radicle {};
+  radicle-httpd   = pkgs.callPackage ./pkgs/radicle-httpd {};
+  rk3588-firmware = pkgs.callPackage ./pkgs/rk3588-firmware {};
+  gigawallet      = pkgs.callPackage ./pkgs/gigawallet {};
 }
---
+++ pkgs/rk3588-firmware/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, stdenv, ... }:
+
+stdenv.mkDerivation rec {
+  pname = "rk3588-firmware";
+  version = "2024.09.14";
+
+  src = fetchGit {
+    url = "https://github.com/friendlyarm/sd-fuse_rk3588.git";
+    rev = "48302f720a1971fcb66bc75edbe624f5e6f9df8c";
+  };
+
+  installPhase = ''
+    mkdir -p $out/etc $out/system $out/lib/firmware
+
+    cp -av prebuilt/firmware/files/etc/*              $out/etc
+    cp -av prebuilt/firmware/files/system/*           $out/system
+    cp -av prebuilt/firmware/files/usr/lib/firmware/* $out/lib/firmware
+
+    cp -av prebuilt/firmware/files2/usr/lib/firmware/regulatory.db     $out/lib/firmware
+    cp -av prebuilt/firmware/files2/usr/lib/firmware/regulatory.db.p7s $out/lib/firmware
+  '';
+
+  meta = {
+    description = "rk3588 Device Firmware";
+    homepage = "https://github.com/friendlyarm/sd-fuse_rk3588";
+    changelog = "https://github.com/friendlyarm/sd-fuse_rk3588/commits/kernel-6.1.y/";
+  };
+}
