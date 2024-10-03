6f694cfd45e0f513c0f9c65c5811851304f16259
Adam Brady <adam@boxxen.org>
2024-10-03 20:11:19 +1000

    update dogenet

--- pkgs/dogenet/default.nix
+++ pkgs/dogenet/default.nix
@@ -5,20 +5,35 @@
   ...
 }:
 
+let
+  geodb = pkgs.fetchurl {
+    url = "https://github.com/sapics/ip-location-db/raw/main/dbip-city/dbip-city-ipv4-num.csv.gz";
+    sha256 = "sha256-Co5NJ1+puxFAubWf289JLolcWxYYkAAb54evAo1mJxs=";
+  };
+in
 buildGoModule {
   pname = "dogenet";
   version = "0.1";
 
-  src = fetchGit {
+  src = pkgs.fetchgit {
     url = "https://github.com/dogeorg/dogenet.git";
-    rev = "610b952f7389b80b894d00f5290c5e0861c429c4";
+    rev = "v0.1.0";
+    hash = "sha256-SQb3aYU6QakkrPpgYRT8KeyITI+Ld99WvL0oo+KGcHg=";
   };
 
-  vendorHash = "sha256-5R+5XQZG6Qjk5m2/Oxq4c4qKbxDcnYz57jdNr8ImcqI=";
+  vendorHash = "sha256-5wV+yH0+ublvXhMpLkru8Y9WCMLy/FK1SG6qozH1CMg=";
 
   nativeBuildInputs = [
     pkgs.go
+    pkgs.gzip
   ];
+
+  postInstall = ''
+    mkdir -p $out/bin/storage
+    cp ${geodb} ./dbip-city-ipv4-num.csv.gz
+    gzip -d dbip-city-ipv4-num.csv.gz
+    mv dbip-city-ipv4-num.csv $out/bin/storage/dbip-city-ipv4-num.csv
+  '';
 
   meta = with lib; {
     description = "Maps the Dogecoin network";
