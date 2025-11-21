04d80480ea6ebd8728e332ea3bd25722092f1838
Ed Tubbs <ectubbs@gmail.com>
2025-11-21 15:45:16 -0600

    Update to v0.1.5-pre-dashb0rd-smpv-stats

--- pkgs/libdogecoin/default.nix
+++ pkgs/libdogecoin/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libdogecoin";
-  version = "0.1.5-pre-dashb0rd-smpv";
+  version = "0.1.5-pre-dashb0rd-smpv-stats";
 
   src = fetchurl {
     url = "https://github.com/edtubbs/libdogecoin/archive/refs/tags/v${version}.tar.gz";
-    hash = "sha256-+FDhDKbrYQyvNgiv6u9VYkZw8TP2UJLz9g6IwYcVHRA=";
+    hash = "sha256-BD6dDl3ny/GaHwnOUDyUSxUr99GLPdPfm/GAmYD478k=";
   };
 
   configurePhase = ''
