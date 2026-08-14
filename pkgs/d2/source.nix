45c0c4d38fa4193db949ab20061eea37d849cd95
Ed Tubbs <ectubbs@gmail.com>
2026-08-15 04:31:42 +1000

    Pin d2 source to f706b26 (08-14-2026)

--- pkgs/d2/source.nix
+++ pkgs/d2/source.nix
@@ -17,14 +17,14 @@
 
 rec {
   # Pinned commit on the copilot/start-testnet-for-d2 branch.
-  rev = "07246524f034c4253fed3359b021b0cd46cfb47b";
-  version = "0-unstable-2026-08-14";
+  rev = "f706b260da4cd6a435875f98163a2bfe830a736a";
+  version = "1-unstable-2026-08-14";
 
   src = pkgs.fetchgit {
     url = "git@github.com:dogecoinfoundation/d2.git";
     inherit rev;
     # To regenerate: nix-prefetch-git git@github.com:dogecoinfoundation/d2.git <rev>
-    hash = "sha256-idtUe2XKNOVwpIuxFoY8LVi2ZgcqK/IzLVPdby920XA=";
+    hash = "sha256-hDKZJkmH0C2nB71jSJzggR2joO/PmMO08XFmpHNLRaI=";
   };
 
   # The three Cargo workspaces in the repository (Makefile targets).
